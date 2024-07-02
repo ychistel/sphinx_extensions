7663703ad09e3e2dc198dfb37fc900e6b91e4369
ychistel <ychistel@ac-caen.fr>
2024-07-02 11:49:20 +0200

    mise à jour workflow

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,7 @@ copyright = year + ',' + author
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_favicon",'sphinx_pyscript','numbered_title']
+extensions = ["sphinx_favicon",'sphinx_pyscript','numbered_title','sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
